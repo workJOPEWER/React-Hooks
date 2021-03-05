1b802e3461812ce7886daf243b96733c52b4d413
workJopewer <arbuztatiana@gmail.com>
2021-03-05 23:52:51 +0200

    custom hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React, {} from 'react';
+import React, {useState, useEffect} from 'react';
 
+//base custom hook
+function useLogger(value) {
+    useEffect(() => {
+        console.log('Value changed :', value)
+    }, [value])
+}
+
+//универсальный код
+function useInput(initialValue) {
+    const [value, setValue] = useState(initialValue);
+
+    const onChange = event => {
+        setValue(event.target.value)
+    };
+
+    const clear = () => setValue(' ')
+    return {
+        bind: {value, onChange},
+        value,
+        clear
+    }
+}
 
 function App() {
-    return (
-        <div>
+    // const [name, setName] = useState(' ')
+    const input = useInput(' ');
 
+    useLogger(input.value);
+
+    return (
+        <div className={'container pt-4'}>
+            {/*<input type="text" value={input.value} onChange={input.onChange}/>*/}
+            {/*ключи совпадают с пораметрами input, поэтому используем синтаксис реакта*/}
+            <input type="text" {...input.bind} />
+            &nbsp;
+            <button onClick={() => input.clear()} className="btn btn-warning btn-sm">Очистить</button>
+            <hr/>
+            <h1>{input.value} </h1>
         </div>
-    );
+    )
 }
 
 export default App;
